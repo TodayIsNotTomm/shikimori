07b5b1e6a3d96e53154ba6b750c7c901602d2eff
j <lastday>
2025-06-02 18:31:48 +0300

    proper topics fix?

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -103,15 +103,19 @@ class TopicsController < ShikimoriController # rubocop:disable Metris/ClassLengt
       params: topic_params
     )
 
-    if @resource.persisted?
-      redirect_to(
-        UrlGenerator.instance.topic_url(@resource),
-        notice: i18n_t('topic.created')
-      )
+    if(@resource)
+      if @resource.persisted?
+        redirect_to(
+          UrlGenerator.instance.topic_url(@resource),
+          notice: i18n_t('topic.created')
+        )
+      else
+        new
+        flash[:alert] = t('changes_not_saved')
+        render :new
+      end
     else
-      new
-      flash[:alert] = t('changes_not_saved')
-      render :new
+      render json: { error: 'Incompatible linked_type or id supplied' }, status: 422
     end
   end
 
